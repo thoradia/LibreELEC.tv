1faa08930db81873ebd2d25dc5986b89b96dda7d
thoradia <22841905+thoradia@users.noreply.github.com>
2020-08-23 01:09:37 +0200

    sonarr: bootstrap

--- packages/thoradia/addons/sonarr/package.mk
+++ packages/thoradia/addons/sonarr/package.mk
@@ -1,10 +1,8 @@
 PKG_NAME="sonarr"
-PKG_VERSION="3.0.3.770"
-PKG_SHA256="84e40e07bcc7b152e5ac65cee639bb7fd6e578587cf957b5d50df1ee829531ea"
-PKG_REV="35"
+PKG_VERSION="bootstrap"
+PKG_REV="39"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://sonarr.tv/"
-PKG_URL="http://download.sonarr.tv/v3/phantom-develop/${PKG_VERSION}/Sonarr.phantom-develop.${PKG_VERSION}.linux.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_TOOLCHAIN="manual"
 
@@ -14,12 +12,10 @@ PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_REQUIRES="virtual.thoradia-mono:0.0.0"
 PKG_SECTION="service"
 PKG_SHORTDESC="${PKG_ADDON_NAME}: a PVR for Usenet and BitTorrent users"
-PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_VERSION}) is a PVR for Usenet and BitTorrent users. It can monitor multiple RSS feeds for new episodes of your favorite shows and will grab, sort and rename them. It can also be configured to automatically upgrade the quality of files already downloaded when a better quality format becomes available."
+PKG_LONGDESC="${PKG_ADDON_NAME} is a PVR for Usenet and BitTorrent users. It can monitor multiple RSS feeds for new episodes of your favorite shows and will grab, sort and rename them. It can also be configured to automatically upgrade the quality of files already downloaded when a better quality format becomes available."
 PKG_MAINTAINER="thoradia"
 PKG_DISCLAIMER="Keep it legal and carry on"
 
 addon() {
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/libs
-    cp -PR ${PKG_BUILD}/* \
-           ${ADDON_BUILD}/${PKG_ADDON_ID}/libs
+  :
 }
--- packages/thoradia/addons/sonarr/source/bin/sonarr.ctl
+++ packages/thoradia/addons/sonarr/source/bin/sonarr.ctl
@@ -1,10 +1,43 @@
 #!/bin/sh
+. /etc/os-release
 . /etc/profile
 oe_setup_addon service.sonarr
 
+download() {
+  _dst=${TMPDIR}/Sonarr
+  _lib=${ADDON_DIR}/libs
+  _url="https://services.sonarr.tv/v1/download/phantom-develop/latest?version=3&os=linux"
+  if [ -e ${_lib} ]; then
+    return
+  fi
+  rm -fr ${_dst}
+  set -o pipefail
+  if ! wget -qO- ${_url} | tar vxzf - -C ${TMPDIR}; then
+    exit 1
+  fi
+  mv ${_dst} ${_lib}
+}
+
+select_runtime() {
+  case ${LIBREELEC_ARCH} in
+    *)
+      download
+      use_mono
+      ;;
+  esac
+}
+
+use_mono() {
+  _cmd="mono --debug ${ADDON_DIR}/libs/Sonarr.exe -data=${ADDON_HOME}"
+  cert-sync --quiet --user /etc/pki/tls/cacert.pem
+}
+
+use_netcore() {
+  export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+}
+
+export TMPDIR=${ADDON_DIR}
 export XDG_CONFIG_HOME=${ADDON_HOME}
-
-cert-sync --quiet --user /etc/pki/tls/cacert.pem
-
+select_runtime
 exec nice -n "${nice:-10}" \
-mono --debug ${ADDON_DIR}/libs/Sonarr.exe -data=${ADDON_HOME} "${@}"
+${_cmd} "${@}"
