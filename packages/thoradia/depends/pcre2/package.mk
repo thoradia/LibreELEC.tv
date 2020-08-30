bf8201d85b9866a4d6fd585f7dd550740772a83d
thoradia <22841905+thoradia@users.noreply.github.com>
2020-08-30 22:35:47 +0200

    pcre2: version 10.35

--- packages/thoradia/depends/pcre2/package.mk
+++ packages/thoradia/depends/pcre2/package.mk
@@ -1,27 +1,13 @@
 PKG_NAME="pcre2"
-PKG_VERSION="10.34"
-PKG_SHA256="74c473ffaba9e13db6951fd146e0143fe9887852ce73406a03277af1d9b798ca"
+PKG_VERSION="10.35"
+PKG_SHA256="9ccba8e02b0ce78046cdfb52e5c177f0f445e421059e43becca4359c669d4613"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.pcre.org/"
-PKG_URL="https://ftp.pcre.org/pub/pcre/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="https://ftp.pcre.org/pub/pcre/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5. PCRE has its own native API, as well as a set of wrapper functions that correspond to the POSIX regular expression API. The PCRE library is free, even for building commercial software."
-PKG_TOOLCHAIN="configure"
+
 PKG_BUILD_FLAGS="+pic"
-
-PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN \
-             --enable-utf8 \
-             --enable-unicode-properties \
-             --with-gnu-ld"
-
-PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-             --enable-static \
-             --enable-utf8 \
-             --enable-pcre2-16 \
-             --enable-unicode-properties \
-             --with-gnu-ld"
-
-post_makeinstall_target() {
-  rm -rf $INSTALL/usr/bin
-  sed -e "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/$PKG_NAME-config
-}
+PKG_CMAKE_OPTS_TARGET="-DPCRE2_BUILD_PCRE2_16=ON \
+                       -DPCRE2_BUILD_PCRE2GREP=OFF \
+                       -DPCRE2_BUILD_TESTS=OFF"
