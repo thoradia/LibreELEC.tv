d0bdb465a76cafc9ed946a7eeb7da08c4e25ea68
thoradia <22841905+thoradia@users.noreply.github.com>
2020-09-09 16:24:39 +0200

    qbittorrent: update qt-everywhere to 5.15.1

--- packages/thoradia/addons/qbittorrent/package.mk
+++ packages/thoradia/addons/qbittorrent/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="qbittorrent"
 PKG_VERSION="4.2.5"
 PKG_SHA256="1dac52d6fe4b0c44dba04fcfc41f519c57a69cb30580255edca95c87053a4324"
-PKG_REV="30"
+PKG_REV="31"
 PKG_LICENSE="GPLv2"
 PKG_SITE="http://www.qbittorrent.org/"
 PKG_URL="https://github.com/qbittorrent/qBittorrent/archive/release-${PKG_VERSION}.tar.gz"
