ea945a9cb97fd2daaa14577ea08a8bccad672b01
thoradia <22841905+thoradia@users.noreply.github.com>
2020-09-22 20:37:11 +0200

    radarr: bootstrap

--- packages/thoradia/addons/radarr/package.mk
+++ packages/thoradia/addons/radarr/package.mk
@@ -1,10 +1,8 @@
 PKG_NAME="radarr"
-PKG_VERSION="0.2.0.1480"
-PKG_SHA256="90982ad0c33df6aa5dc0ba26874b7b53ace13fcea700ac4268f146315dcad318"
-PKG_REV="26"
+PKG_VERSION="bootstrap"
+PKG_REV="34"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/Radarr/Radarr"
-PKG_URL="https://github.com/Radarr/Radarr/releases/download/v${PKG_VERSION}/Radarr.develop.${PKG_VERSION}.linux.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_TOOLCHAIN="manual"
 
@@ -14,12 +12,14 @@ PKG_ADDON_TYPE="xbmc.service"
 PKG_ADDON_REQUIRES="virtual.thoradia-mono:0.0.0"
 PKG_SECTION="service"
 PKG_SHORTDESC="${PKG_ADDON_NAME}: a fork of Sonarr to work with movies à la CouchPotato"
-PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_VERSION}) is a fork of Sonarr to work with movies à la CouchPotato."
+PKG_LONGDESC="${PKG_ADDON_NAME} is a fork of Sonarr to work with movies à la CouchPotato."
 PKG_MAINTAINER="thoradia"
 PKG_DISCLAIMER="Keep it legal and carry on"
 
+if [ "${DEVICE}" == "RPi" ]; then
+  PKG_ADDON_REQUIRES="tools.mono:0.0.0 ${PKG_ADDON_REQUIRES}"
+fi
+
 addon() {
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/libs
-    cp -PR ${PKG_BUILD}/* \
-           ${ADDON_BUILD}/${PKG_ADDON_ID}/libs
+  :
 }
