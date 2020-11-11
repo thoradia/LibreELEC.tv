87dc23636fe5a36efd8e4c7fb567e86196c37255
thoradia <22841905+thoradia@users.noreply.github.com>
2020-11-11 10:22:30 +0100

    flexget: version 3.1.88

--- packages/thoradia/addons/flexget/package.mk
+++ packages/thoradia/addons/flexget/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="flexget"
-PKG_VERSION="3.1.84"
-PKG_REV="20"
+PKG_VERSION="3.1.88"
+PKG_REV="21"
 PKG_LICENSE="MIT"
 PKG_SITE="https://flexget.com"
 PKG_DEPENDS_TARGET="toolchain crossenv:host libyaml"
